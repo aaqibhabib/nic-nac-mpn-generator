a0840e65a81705b493bfe704a2aaf6db36578855
Aaqib Habib <aaqib.habib@gmail.com>
2017-02-25 21:27:14 -0500

    updates

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import Index from 'components/Index';
 
-render(<Index />, document.getElementById('js-main'));
+ReactDOM.render(<Index />, document.getElementById('js-main'));
