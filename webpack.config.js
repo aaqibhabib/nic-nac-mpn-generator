94155c688052c9d05b7f4b04e3f6f4a35ec4c553
Aaqib Habib <aaqib.habib@gmail.com>
2017-03-04 09:56:33 -0500

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,40 @@
-module.exports = process.env.NODE_ENV === 'production' ? require('./webpack.config.production.js') : require('./webpack.config.base.js');
+const path = require('path');
+
+module.exports = {
+    target: 'web',
+
+    entry: [
+        'babel-polyfill',
+        './src/main.jsx',
+    ],
+
+    output: {
+        path: path.join(process.cwd(), '/dist'),
+        pathInfo: true,
+        publicPath: '/dist/',
+        filename: 'main.js',
+    },
+
+    resolve: {
+        root: path.join(__dirname, ''),
+        modulesDirectories: [
+            'node_modules',
+            'src',
+        ],
+        extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx'],
+    },
+
+    plugins: [],
+
+    module: {
+        loaders: [
+            { test: /\.json$/, loader: 'json-loader' },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel'],
+                exclude: /node_modules/,
+            }],
+
+        noParse: /\.min\.js/,
+    },
+};
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,47 +1,28 @@
-'use strict';
-
-var webpack = require('webpack');
-var config = require('./webpack.config.base.js');
-
-var SaveAssetsJson = require('assets-webpack-plugin');
+const webpack = require('webpack');
+const config = require('./webpack.config.js');
 
 config.bail = true;
 config.debug = false;
 config.profile = false;
 config.devtool = '#source-map';
 
-config.output = {
-  path: './client/dist',
-  pathInfo: true,
-  publicPath: '/client/dist/',
-  filename: 'bundle.[hash].min.js'
-};
-
 config.plugins = config.plugins.concat([
-  new webpack.optimize.OccurenceOrderPlugin(true),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    output: {
-      comments: false
-    },
-    compress: {
-      warnings: false,
-      screw_ie8: true
-    }
-  }),
-  new SaveAssetsJson({
-    path: process.cwd(),
-    filename: 'assets.json'
-  }),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production')
-    }
-  })
-]);
-
-config.module.loaders = config.module.loaders.concat([
-  {test: /\.jsx?$/, loaders: [ 'babel'], exclude: /node_modules/}
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        output: {
+            comments: false,
+        },
+        compress: {
+            warnings: false,
+            screw_ie8: true,
+        },
+    }),
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+        },
+    }),
 ]);
 
 module.exports = config;
