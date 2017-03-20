eb0cdaf06338b97236e6845789e834e30e1f3ef5
Aaqib Habib <aaqib.habib@gmail.com>
2017-03-19 20:16:50 -0400

    lint fix

--- client/main.jsx
+++ client/main.jsx
@@ -4,8 +4,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from 'components/App';
 
-// Needed for onTouchTap 
-// http://stackoverflow.com/a/34015469/988941 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('js-main'));
