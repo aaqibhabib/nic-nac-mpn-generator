2297d560285a24ac25207e04f0c08d80276b23ec
amyfried <amyfried@users.noreply.github.com>
2017-04-03 17:43:41 -0400

    updating intro page

--- client/components/Intro.jsx
+++ client/components/Intro.jsx
@@ -15,14 +15,14 @@ export default class Intro extends React.Component {
     render() {
         return (
             <div>
-                <Card style={{ margin: '21.440 0' }}>
-                    <CardTitle><h1 style={{ margin: 0 }} >Create Your Privacy Notice</h1></CardTitle>
-                    <CardText>
-                        <p>This tool will help you generate a <a href="https://www.healthit.gov/sites/default/files/2016_model_privacy_notice.pdf">Model Privacy Notice (MPN)</a> for your company. After answering a few simple questions about your company’s privacy and security practices, you’ll be able to download HTML for your MPN to use on your website.</p>
+                <h2 style={{margin: 0 }} >Create Your Privacy Notice</h2>
+                    <p>Help your users understand how your organization
+                             collects and shares information. Answer a couple
+                              questions about your privacy policy and make a
+                               &quot;In Plain English&quot; policy to share. </p>
+                    <p>This tool will help you generate a <a href="https://www.healthit.gov/sites/default/files/2016_model_privacy_notice.pdf">Model Privacy Notice (MPN)</a> for your company. After answering a few simple questions about your company’s privacy and security practices, you’ll be able to download HTML for your MPN to use on your website.</p>
 
-                        <p>It’d be helpful to have your company’s full-length privacy policy on hand or assistance from your company's legal team to answer these questions.</p>
-                    </CardText>
-                </Card>
+                    <p>It’d be helpful to have your company’s full-length privacy policy on hand or assistance from your company's legal team to answer these questions.</p>
             </div>
         );
     }
