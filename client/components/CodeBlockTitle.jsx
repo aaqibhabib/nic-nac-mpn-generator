787c30b3bfa8c0b65eeb9a8438b2a240b02a649a
Aaqib Habib <aaqib.habib@gmail.com>
2017-04-05 23:07:34 -0400

    Adding Icons

--- client/components/CodeBlockTitle.jsx
+++ client/components/CodeBlockTitle.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import IconButton from 'material-ui/IconButton';
 import CodeIcon from 'material-ui/svg-icons/action/code';
+import DownloadIcon from 'material-ui/svg-icons/file/file-download';
+import CopyIcon from 'material-ui/svg-icons/content/content-copy';
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
 
 const CodeBlockTitle = props => (
@@ -9,6 +11,12 @@ const CodeBlockTitle = props => (
             <ToolbarTitle text={props.title || 'Example'} />
         </ToolbarGroup>
         <ToolbarGroup>
+            <IconButton touch tooltip={'Download'}>
+                <DownloadIcon />
+            </IconButton>
+            <IconButton touch tooltip={'Copy to clipboard'}>
+                <CopyIcon />
+            </IconButton>
             <IconButton touch tooltip={props.tooltip}>
                 <CodeIcon />
             </IconButton>
