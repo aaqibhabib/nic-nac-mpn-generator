6d5a0268278b51da98aee4ffaf2d7217a42714f8
Aaqib Habib <aaqib.habib@gmail.com>
2017-04-05 23:54:36 -0400

    got copy to work

--- client/components/CodeBlock.jsx
+++ client/components/CodeBlock.jsx
@@ -30,6 +30,32 @@ const styles = {
   },
 };
 
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = 0;
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+}
+
 class CodeBlock extends Component {
   static propTypes = {
     children: PropTypes.string,
@@ -47,7 +73,7 @@ class CodeBlock extends Component {
     });
   };
   onCopy = e => {
-    debugger;
+    copyTextToClipboard(this.props.children);
   };
   onDownload = e => {
     const blob = new Blob([this.props.children], {type : 'text/html'});
